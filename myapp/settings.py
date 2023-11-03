f027946a0b0f900e3bab43030dd5e128fb91f955
uyenbhku <uyen.bhku@gmail.com>
2023-11-03 16:25:18 +0700

    temporarily turn off debug

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut aw
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'uyenbhku.up.railway.app',
